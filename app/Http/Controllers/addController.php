cdb9af9a82594721b35332d44f189ebac720402f
TECHIS-teamhiroshima <moto.03tennis@icloud.com>
2022-09-12 17:01:44 +0900

    一応

--- app/Http/Controllers/addController.php
+++ app/Http/Controllers/addController.php
@@ -8,24 +8,24 @@ use App\Models\add;
 
 class addController extends Controller
 {
-    // addージの表示
+        // addージの表示
     public function add()
     {
         return view('add.add');
     }
 
-    // 投稿された内容を表示するページ
-    public function create(Request $request) {
+        // 投稿された内容を表示するページ
+        public function create(Request $request) {
 
-        // 投稿内容の受け取って変数に入れる
-        $name = $request->input('name');
-        $type = $request->input('type');
+            // 投稿内容の受け取って変数に入れる
+            $name = $request->input('name');
+            $type = $request->input('type');
 
-        // 変数をビューに渡す
-        return view('add.blade.php')->with([
-            "name" => $name,
-            "type"  => $type,
-        ]);
-    }
+            // 変数をビューに渡す
+            return view('add.blade.php')->with([
+                "name" => $name,
+                "type"  => $type,
+            ]);
+}
 
 }
