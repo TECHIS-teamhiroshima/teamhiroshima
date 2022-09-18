3161e1a501355e6c4826b79853c1cb3a5faf6ec8
shocho <the.principal.of.himajinschool@gmail.com>
2022-09-18 12:39:03 +0900

    更新〜2

--- app/Http/Controllers/addController.php
+++ app/Http/Controllers/addController.php
@@ -13,20 +13,3 @@ class AddController extends Controller
         return view('add.add');
     }
 }
-
-
-// class AddController extends Controller
-// {
-//     public function upload(Request $request)
-//     {
-//         // ディレクトリ名
-//         $dir = 'sample';
-
-//         // アップロードされたファイル名を取得
-//         $file_name = $request->file('image')->getClientOriginalName();
-
-//         // 取得したファイル名で保存
-//         $request->file('image')->storeAs('public/' . $dir, $file_name);
-
-//         return redirect('/');
-//     }
