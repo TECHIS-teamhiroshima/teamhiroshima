39c2bd22e6aefe480ae95eaa8bbb1c8fe9b0e3be
TECHIS-teamhiroshima <moto.03tennis@icloud.com>
2022-09-22 20:12:23 +0900

    IDに4が付与される調査

--- app/Http/Controllers/AddController.php
+++ app/Http/Controllers/AddController.php
@@ -14,6 +14,7 @@ class AddController extends Controller
 {
     public function add()
     {
+        dd('id');
         $id=  Auth::id();
         return view('add.add', [
             "user_id" => $id,
