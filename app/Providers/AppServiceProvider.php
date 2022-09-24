99bf9c4468d00056829f2a418fa28c363283fd31
shocho <the.principal.of.himajinschool@gmail.com>
2022-09-24 20:15:25 +0900

    style修正2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,6 +40,6 @@ class AppServiceProvider extends ServiceProvider
     // }
     public function boot(UrlGenerator $url)
     {
-        $url->forceScheme('https');
+        $url->forceScheme('http');
     }
 }
